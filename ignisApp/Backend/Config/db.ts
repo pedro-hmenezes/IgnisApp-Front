55c1463b5a2b28fffc81ba2411d23891e2bbd838
Maria Augusta Fialho <93688427+22augusta@users.noreply.github.com>
2025-10-22 17:27:53 +0000

    Tentando integrar o db
    Signed-off-by: GitHub <noreply@github.com>

--- ignisApp/Backend/Config/db.ts
+++ ignisApp/Backend/Config/db.ts
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI!);
-        console.log('MongoDB conectado com sucesso!');
-    } catch (error) {
-        console.error('Erro ao conectar ao MongoDB:', error);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI!);
+    console.log('MongoDB conectado com sucesso!');
+  } catch (error) {
+    console.error('Erro ao conectar ao MongoDB:', error);
+    process.exit(1);
+  }
 };
